4f4a5186de5e3955810b3c76b7a4b0b07d1c960b
Hamza Bhatti <bhatti3993@gmail.com>
2024-12-28 01:39:47 +0500

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,6 @@ export default function Home() {
       {/* <Image src={"/images/hero.jpg"} alt="Hero" width={100} height={100} className="w-full rounded-lg mb-[-10px]"/> */}
       <div className="bg-[url('/images/hero.jpg')] h-[500px] w-full bg-no-repeat bg-cover rounded-lg opacity-75">
       </div>
-      <div className="w-1/3 mx-auto  bg-white">
-<h5>Technology</h5>
-
-</div>
-
     </div>
 
 <BlogCard />
